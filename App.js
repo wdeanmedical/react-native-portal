66824cb33949ee992186e7a141af0860a08672ba
Nick Sophinos <nick@wdeanmedical.com>
2019-06-15 10:13:09 -0400

    updated starting point

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up Crap.js to start working on your app!</Text>
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -9,13 +17,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 })
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    )
-  }
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# react-native-portal
-WDM Client Portal Created In React Native
-
-This project features Expo and related tools.
